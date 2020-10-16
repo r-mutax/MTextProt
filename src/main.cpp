e21db9df9c4ecd543f2fd2b8052fd67342a20170
syarin-x <magicspell.max.mix.mutax@gmail.com>
2020-10-17 00:16:30 +0900

    push test2

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]){
 
     printf(clr);                // 画面クリア
     printf("\e[1;1H");          // カーソルを左上に
-    printf("\e[31m\e[47m");     // color　文字を赤、波形を白
+    printf("\e[31m\e[47m");     // color　文字を赤、波形を白に
     printf("hello,world!\n");   
     printf("\e[0m\e[0m");
 
