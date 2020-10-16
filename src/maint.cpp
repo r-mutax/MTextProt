a61488ed146b3a8c71683aff27dda54286ba0047
syarin-x <magicspell.max.mix.mutax@gmail.com>
2020-10-16 22:42:02 +0900

    first commit

---
+++ src/maint.cpp
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define clr             "\e[2J"
+
+int main(int argc, char *argv[]){
+
+    printf(clr);                // 画面クリア
+    printf("\e[1;1H");          // カーソルを左上に
+    printf("\e[31m\e[47m");     // color　文字を赤、波形を白
+    printf("hello,world!\n");   
+    printf("\e[0m\e[0m");
+
+    for(int i=0; i<argc; i++) {
+        printf("receive ! > %s \n",argv[i]);
+    }
+    return 0;
+}
